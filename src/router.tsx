adb59e94d54268577b67a4786b18a6a8128a8be6
IstsHitori <sernaojuliofranciscojavier@gmail.com>
2025-12-06 22:33:37 -0500

    saving changes

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import { AuhtLayout, RootLayout } from "./layouts";
 import { LoginForm } from "./features/auth/components/LoginForm";
+import HomePage from "./features/home-dashboard/pages/HomePage";
 
 export const router = createBrowserRouter([
   {
@@ -11,6 +12,11 @@ export const router = createBrowserRouter([
   {
     path: "/app",
     element: <RootLayout />,
-    children: [],
+    children: [
+      {
+        path: "home",
+        element: <HomePage />,
+      },
+    ],
   },
 ]);
